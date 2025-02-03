6b1ccd2c449d029362d6cb5f79ef185f74de45bc
John Paul Francsico <franciscojpz101@gmail.com>
2025-02-04 04:43:44 +0800

    Finished top part of Body component

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,13 +1,29 @@
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { SearchIcon, Plus } from "lucide-react";
+
 export default function Body() {
   return (
-    <main className="p-4">
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
+    <main>
+      <div className="flex justify-between items-center p-5 h-20 border-b">
+        <h1 className="text-3xl font-bold text-primary">Kanban Board</h1>
+        <div className="flex items-center space-x-4">
+          <div className="relative">
+            <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+            <Input
+              type="text"
+              placeholder="Search"
+              className="px-3 py-1 border rounded-md w-56 text-primary font-normal text-sm pl-10"
+            />
+          </div>
+          <Button className="bg-[#0786FD] text-primary-foreground px-4 py-2 rounded-md w-32 items-center justify-center">
+            <Plus className="!h-5 !w-5" />
+            <span>New Lead</span>
+          </Button>
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </div>
+      <div className="flex justify-between items-center p-5 h-20">
+        Cold Owner
       </div>
     </main>
   );
