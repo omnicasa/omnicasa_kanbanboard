909aaab78b2a5ea5de9fd90621c999fab3cf7e92
John Paul Francsico <franciscojpz101@gmail.com>
2025-02-04 09:12:22 +0800

    [WIP] tabs part, custom select component

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,6 +1,16 @@
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { SearchIcon, Plus } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Body() {
   return (
@@ -22,9 +32,119 @@ export default function Body() {
           </Button>
         </div>
       </div>
-      <div className="flex justify-between items-center p-5 h-20">
-        Cold Owner
-      </div>
+      <Tabs defaultValue="cold_owner">
+        <div className="flex justify-between items-center p-5">
+          <TabsList>
+            <TabsTrigger
+              value="cold_owner"
+              className="text-muted-foreground data-[state=active]:text-primary"
+            >
+              Cold Owner
+            </TabsTrigger>
+            <TabsTrigger
+              value="cold_adoption"
+              className="text-muted-foreground data-[state=active]:text-primary"
+            >
+              Cold Adoption
+            </TabsTrigger>
+            <TabsTrigger
+              value="warm_bought"
+              className="text-muted-foreground data-[state=active]:text-primary"
+            >
+              Warm Bought
+            </TabsTrigger>
+            <TabsTrigger
+              value="warm_leads"
+              className="text-muted-foreground data-[state=active]:text-primary"
+            >
+              Warm Leads
+            </TabsTrigger>
+          </TabsList>
+          <div className="flex items-center space-x-2">
+            <Select>
+              <SelectTrigger className="w-[206px] px-4 text-primary font-medium text-sm">
+                <span className="max-w-[150px] truncate !block">
+                  <SelectValue placeholder="Greenfield Realty - Oakwood - Antwerp" />
+                </span>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Site</SelectLabel>
+                  <SelectItem value="antwerp">
+                    Greenfield Realty - Oakwood - Antwerp
+                  </SelectItem>
+                  <SelectItem value="ghent">
+                    Greenfield Realty - Oakwood - Ghent
+                  </SelectItem>
+                  <SelectItem value="bruges">
+                    Greenfield Realty - Oakwood - Bruges
+                  </SelectItem>
+                  <SelectItem value="namur">
+                    Greenfield Realty - Oakwood - Namur
+                  </SelectItem>
+                  <SelectItem value="liege">
+                    Greenfield Realty - Oakwood - Liege
+                  </SelectItem>
+                  <SelectItem value="leuven">
+                    Greenfield Realty - Oakwood - Leuven
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Select>
+              <SelectTrigger className="w-[180px] px-4 text-primary font-medium text-sm">
+                <SelectValue placeholder="John Doe" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Person</SelectLabel>
+                  <SelectItem value="John Doe">John Doe</SelectItem>
+                  <SelectItem value="Raphael Aerts">Raphael Aerts</SelectItem>
+                  <SelectItem value="Stephane Anciaux">
+                    Stephane Anciaux
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Select>
+              <SelectTrigger className="w-[94px] px-4 text-primary font-medium text-sm">
+                <SelectValue placeholder="Filter" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Lead Source</SelectLabel>
+                  <SelectItem value="lead_source">Lead Source</SelectItem>
+                  <SelectItem value="source">Source</SelectItem>
+                  <SelectItem value="notary">Notary</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <Select>
+              <SelectTrigger className="max-w-[200px] px-4 text-primary font-medium text-sm">
+                <SelectValue placeholder="Sort by" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="most_updated">
+                  Most recently updated
+                </SelectItem>
+                <SelectItem value="least_updated">
+                  Least recently updated
+                </SelectItem>
+                <SelectItem value="most_added">Most recently added</SelectItem>
+                <SelectItem value="least_added">
+                  Least recently added
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="px-5">
+          <TabsContent value="cold_owner">Cold Owner part</TabsContent>
+          <TabsContent value="cold_adoption">Cold Adoption part</TabsContent>
+          <TabsContent value="warm_bought">Warm Bought part</TabsContent>
+          <TabsContent value="warm_leads">Warm Leads part</TabsContent>
+        </div>
+      </Tabs>
     </main>
   );
 }
