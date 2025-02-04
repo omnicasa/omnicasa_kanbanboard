c94996a800205b2360dc71900080257ea68786b8
John Paul Francsico <franciscojpz101@gmail.com>
2025-02-04 17:29:32 +0800

    Finished Custom SelectSite component

--- components/SelectSite.tsx
+++ components/SelectSite.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React, { useState } from "react";
-
+import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
@@ -18,6 +17,7 @@ const locations = ["Antwerp", "Ghent", "Bruges", "Namur", "Liege", "Leuven"];
 export default function SelectSite() {
   const [isCommandVisible, setIsCommandVisible] = useState(false);
   const [selectedLocations, setSelectedLocations] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const toggleCommandVisibility = () => {
     setIsCommandVisible(!isCommandVisible);
@@ -33,16 +33,27 @@ export default function SelectSite() {
     });
   };
 
+  const handleSearchChange = (value: string) => {
+    setSearchTerm(value);
+  };
+
+  const filteredLocations = locations.filter((location) =>
+    `Greenfield Realty - Oakwood - ${location}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+
   const selectedCount = selectedLocations.length;
   const firstSelectedLocation =
-    `Greenfield Realty - Oakwood - ${selectedLocations[0]}` ||
-    "Select a location";
+    selectedLocations.length === 0
+      ? "Select a location"
+      : `Greenfield Realty - Oakwood - ${selectedLocations[0]}`;
 
   return (
     <div className="relative">
       <Button
         variant="outline"
-        className="inline-flex h-9 px-4 py-2 items-center gap-2 flex-shrink-0 rounded-md border text-primary bg-white shadow-sm"
+        className="inline-flex h-9 px-4 py-2 items-center gap-2 flex-shrink-0 rounded-md border text-primary bg-white shadow-sm font-sans"
         onClick={toggleCommandVisibility}
       >
         <div className="flex items-center gap-1 max-w-[150px]">
@@ -59,31 +70,38 @@ export default function SelectSite() {
       </Button>
       {isCommandVisible && (
         <Command className="absolute left-0 mt-1 rounded-lg border shadow-md md:min-w-[334px] h-auto">
-          <CommandInput placeholder="Site" />
+          <CommandInput
+            placeholder="Site"
+            value={searchTerm}
+            onValueChange={(value: string) => handleSearchChange(value)}
+          />
           <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            {locations.map((location) => (
-              <CommandItem key={location.toLowerCase()}>
-                <div className="flex items-center gap-[10px] self-stretch px-1">
-                  <Checkbox
-                    id={location.toLowerCase()}
-                    checked={selectedLocations.includes(location)}
-                    onCheckedChange={() => handleCheckboxChange(location)}
-                    className={`flex w-4 items-start gap-2 ${
-                      selectedLocations.includes(location)
-                        ? "!bg-[#0786fd] border-transparent"
-                        : ""
-                    }`}
-                  />
-                  <label
-                    htmlFor={location.toLowerCase()}
-                    className="text-sm font-normal leading-[1.42857] text-primary overflow-hidden text-ellipsis"
-                  >
-                    Greenfield Realty - Oakwood - {location}
-                  </label>
-                </div>
-              </CommandItem>
-            ))}
+            {filteredLocations.length === 0 ? (
+              <CommandEmpty>No results found.</CommandEmpty>
+            ) : (
+              filteredLocations.map((location: string) => (
+                <CommandItem key={location.toLowerCase()}>
+                  <div className="flex items-center gap-[10px] self-stretch px-1">
+                    <Checkbox
+                      id={location.toLowerCase()}
+                      checked={selectedLocations.includes(location)}
+                      onCheckedChange={() => handleCheckboxChange(location)}
+                      className={`flex w-4 items-start gap-2 ${
+                        selectedLocations.includes(location)
+                          ? "!bg-[#0786fd] border-transparent"
+                          : "border-[#E4E4E7]"
+                      }`}
+                    />
+                    <label
+                      htmlFor={location.toLowerCase()}
+                      className="text-sm font-normal leading-[1.42857] text-primary overflow-hidden text-ellipsis"
+                    >
+                      Greenfield Realty - Oakwood - {location}
+                    </label>
+                  </div>
+                </CommandItem>
+              ))
+            )}
           </CommandList>
         </Command>
       )}
