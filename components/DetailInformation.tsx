a65880550a96c52518cae649a916c4d096dd9cad
John Paul Francsico <franciscojpz101@gmail.com>
2025-02-11 09:32:39 +0800

    - update DetailInformation component(add badge click function)

--- components/DetailInformation.tsx
+++ components/DetailInformation.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { Badge } from "./ui/badge";
 import {
   ChevronsUpDown,
@@ -13,11 +15,89 @@ import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 
 const DetailInformation: React.FC = () => {
+  const [proprietor, setProprietor] = useState(false);
+  const [seller, setSeller] = useState(false);
+  const [corporate, setCorporate] = useState(false);
+  const handleProprietor = () => {
+    setProprietor(!proprietor);
+  };
+  const handleSeller = () => {
+    setSeller(!seller);
+  };
+  const handleCorporate = () => {
+    setCorporate(!corporate);
+  };
   return (
     <div className="flex flex-col itesm-start w-[324px] p-5 gap-5 bg-white border rounded-lg shadow-md h-full">
       <h2 className="text-card-foreground text-base font-semibold leading-normal font-sans">
         Relations
       </h2>
+      {proprietor && (
+        <>
+          <div
+            onClick={() => handleProprietor()}
+            className="flex items-center justify-between gap-2 w-full cursor-pointer"
+          >
+            <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
+              Amélie Smet
+            </h2>
+            <div className="flex items-center gap-2">
+              <Badge
+                variant="secondary"
+                className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+              >
+                Proprietors
+              </Badge>
+              <ChevronsUpDown width={16} height={16} className="opacity-50" />
+            </div>
+          </div>
+          <Separator className="border" />
+        </>
+      )}
+      {seller && (
+        <>
+          <div
+            onClick={() => handleSeller()}
+            className="flex items-center justify-between gap-2 w-full cursor-pointer"
+          >
+            <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
+              Jeanne Reynders
+            </h2>
+            <div className="flex items-center gap-2">
+              <Badge
+                variant="secondary"
+                className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+              >
+                Notary/Seller
+              </Badge>
+              <ChevronsUpDown width={16} height={16} className="opacity-50" />
+            </div>
+          </div>
+          <Separator className="border" />
+        </>
+      )}
+      {corporate && (
+        <>
+          <div
+            onClick={() => handleCorporate()}
+            className="flex items-center justify-between gap-2 w-full cursor-pointer"
+          >
+            <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
+              Alexandre Arnaud
+            </h2>
+            <div className="flex items-center gap-2">
+              <Badge
+                variant="secondary"
+                className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+              >
+                Body corporate
+              </Badge>
+              <ChevronsUpDown width={16} height={16} className="opacity-50" />
+            </div>
+          </div>
+          <Separator className="border" />
+        </>
+      )}
       <div className="flex gap-2 items-center">
         <Image src="/images/avatar.png" alt="avatar" width={48} height={48} />
         <div className="flex flex-col gap-1 align-start">
@@ -110,51 +190,72 @@ const DetailInformation: React.FC = () => {
           </h3>
         </div>
       </div>
-      <Separator className="border" />
-      <div className="flex items-center justify-between gap-2 w-full">
-        <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
-          Amélie Smet
-        </h2>
-        <div className="flex items-center gap-2">
-          <Badge
-            variant="secondary"
-            className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+      {!proprietor && (
+        <>
+          <Separator className="border" />
+          <div
+            onClick={() => handleProprietor()}
+            className="flex items-center justify-between gap-2 w-full cursor-pointer"
           >
-            Proprietors
-          </Badge>
-          <ChevronsUpDown width={16} height={16} className="opacity-50" />
-        </div>
-      </div>
-      <Separator className="border" />
-      <div className="flex items-center justify-between gap-2 w-full">
-        <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
-          Jeanne Reynders
-        </h2>
-        <div className="flex items-center gap-2">
-          <Badge
-            variant="secondary"
-            className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+            <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
+              Amélie Smet
+            </h2>
+            <div className="flex items-center gap-2">
+              <Badge
+                variant="secondary"
+                className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+              >
+                Proprietors
+              </Badge>
+              <ChevronsUpDown width={16} height={16} className="opacity-50" />
+            </div>
+          </div>
+        </>
+      )}
+      {!seller && (
+        <>
+          <Separator className="border" />
+          <div
+            onClick={() => handleSeller()}
+            className="flex items-center justify-between gap-2 w-full cursor-pointer"
           >
-            Notary/Seller
-          </Badge>
-          <ChevronsUpDown width={16} height={16} className="opacity-50" />
-        </div>
-      </div>
-      <Separator className="border" />
-      <div className="flex items-center justify-between gap-2 w-full">
-        <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
-          Alexandre Arnaud
-        </h2>
-        <div className="flex items-center gap-2">
-          <Badge
-            variant="secondary"
-            className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+            <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
+              Jeanne Reynders
+            </h2>
+            <div className="flex items-center gap-2">
+              <Badge
+                variant="secondary"
+                className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+              >
+                Notary/Seller
+              </Badge>
+              <ChevronsUpDown width={16} height={16} className="opacity-50" />
+            </div>
+          </div>
+        </>
+      )}
+      {!corporate && (
+        <>
+          <Separator className="border" />
+          <div
+            onClick={() => handleCorporate()}
+            className="flex items-center justify-between gap-2 w-full cursor-pointer"
           >
-            Body corporate
-          </Badge>
-          <ChevronsUpDown width={16} height={16} className="opacity-50" />
-        </div>
-      </div>
+            <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
+              Alexandre Arnaud
+            </h2>
+            <div className="flex items-center gap-2">
+              <Badge
+                variant="secondary"
+                className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+              >
+                Body corporate
+              </Badge>
+              <ChevronsUpDown width={16} height={16} className="opacity-50" />
+            </div>
+          </div>
+        </>
+      )}
       <Button
         variant="outline"
         className="flex h-9 px-4 py-2 justify-center items-center gap-2 self-stretch rounded-md border bg-white shadow-sm"
