bc5375b5210e3ad369d9ad2c589cc7605ff5de52
John Paul Francsico <franciscojpz101@gmail.com>
2025-02-13 15:22:56 +0800

    [WIP] DetailInformation API integration(debugging of person information API integration)

--- components/DetailInformation.tsx
+++ components/DetailInformation.tsx
@@ -19,6 +19,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { useFetchPersonInfo } from "@/hooks/useFetchData";
 
 interface Relation {
   Id: number;
@@ -26,14 +27,194 @@ interface Relation {
   RelationTypeNameEN: string;
   PersonEmail: string;
   PhoneNumber1: string;
-  PropertyId: number;
+  PersonId: number;
 }
+
+interface Mult {
+  Id: number;
+  PersonId: number;
+  FieldType: number;
+  Value: number;
+  Main: boolean;
+  ItemState: number;
+}
+
+interface SiteInfo {
+  Id: number;
+  SiteName: string;
+  CompanyName: string;
+  Street: string;
+  Nr: string;
+  PostalCode: string;
+  City: string;
+  Country: string;
+  Phone: string;
+  Email: string;
+  Website: string;
+}
+
+interface ManagerInfo {
+  Id: number;
+  Name: string;
+  ShortName: string;
+  Email: string;
+  GroupId: number;
+  DepartmentId: number;
+  SiteId: number;
+  IsActive: boolean;
+}
+
+interface StatusInfo {
+  Id: number;
+  NameNL: string;
+  NameFR: string;
+  NameEN: string;
+  NameDE: string;
+}
+
+interface DepartmentInfo {
+  Id: number;
+  NameNL: string;
+  NameFR: string;
+  NameEN: string;
+}
+
+interface PreferredMediaInfo {
+  Id: number;
+  NameNL: string;
+  NameFR: string;
+  NameEN: string;
+  NameDE: string;
+}
+
+interface PreferredConfirmationInfo {
+  Id: number;
+  NameNL: string;
+  NameFR: string;
+  NameEN: string;
+  NameDE: string;
+}
+
+interface OriginContactInfo {
+  Id: number;
+  NameNL: string;
+  NameFR: string;
+  NameEN: string;
+  NameDE: string;
+}
+
+interface DocumentTypeInfo {
+  Id: number;
+  NameNL: string;
+  NameFR: string;
+  NameEN: string;
+  NameDE: string;
+}
+
+interface PublishOnInternetInfo {
+  Id: number;
+  NameNL: string;
+  NameFR: string;
+  NameEN: string;
+  NameDE: string;
+}
+interface Document {
+  Comments: string;
+  CreatedDate: string;
+  DocumentTypeId: number;
+  DocumentTypeInfo: DocumentTypeInfo;
+  FilePath: string;
+  Filename: string;
+  GroupId: number;
+  IconURL: string;
+  Id: number;
+  IsConvertToPDF: boolean;
+  IsExternalDocument: boolean;
+  IsFolder: boolean;
+  IsIncludeInMail: boolean;
+  IsMissingOnCloud: boolean;
+  ItemState: number;
+  ModifiedDate: string;
+  PropertyId: number;
+  PublishOnInternetId: number;
+  PublishOnInternetInfo: PublishOnInternetInfo;
+  URL: string;
+}
+
+interface PersonInfo {
+  EnteredDate: string;
+  StatusId: number;
+  EnteredId: number;
+  CountryCode: string;
+  Website: string;
+  AltCountryCode: string;
+  LastChange: string;
+  LastChangedBy: number;
+  CategoriesDesc: string;
+  ManagerId: number;
+  CityPostcode: string;
+  CityName: string;
+  PreferredMediaId: number;
+  Alt2CountryCode: string;
+  PhoneDesc1: string;
+  PhoneDesc2: string;
+  PhoneDesc3: string;
+  PhoneNumber1: string;
+  Password: string;
+  ModifiedDate: string;
+  SiteId: number;
+  CountryId: number;
+  CountryId2: number;
+  CountryId3: number;
+  HasPic: boolean;
+  OriginContactId: number;
+  DepartmentId: number;
+  PreferredConfirmationId: number;
+  AddressAGoogleX: number;
+  AddressAGoogleY: number;
+  MiddleName: string;
+  Gender: number;
+  Mults: Mult[];
+  Relations: Relation[];
+  Documents: Document[];
+  CountryName: string;
+  CountryName2: string;
+  CountryName3: string;
+  SiteInfo: SiteInfo;
+  ManagerInfo: ManagerInfo;
+  StatusInfo: StatusInfo;
+  DepartmentInfo: DepartmentInfo;
+  PreferredMediaInfo: PreferredMediaInfo;
+  PreferredConfirmationInfo: PreferredConfirmationInfo;
+  OriginContactInfo: OriginContactInfo;
+  LastModifiedDate: string;
+  Id: number;
+  Name: string;
+  Reference: string;
+  FirstName: string;
+  AddressTitle: string;
+  LetterTitle: string;
+  Fax: string;
+  AddressA: string;
+  Language: keyof typeof languageEnum;
+  Email: string;
+  IsDeleted: boolean;
+}
+
+export type { PersonInfo };
 
 interface DetailInformationProps {
   data: {
     Relations: Relation[];
   };
 }
+
+const languageEnum = {
+  1: "Dutch",
+  2: "French",
+  3: "English",
+  4: "German",
+};
 
 const DetailInformation: React.FC<DetailInformationProps> = ({ data }) => {
   const [proprietor, setProprietor] = useState(false);
@@ -50,6 +231,13 @@ const DetailInformation: React.FC<DetailInformationProps> = ({ data }) => {
   const [triggerVisible, setTriggerVisible] = useState<{
     [key: string]: boolean;
   }>(initialTriggerVisible);
+  const [selectedId, setSelectedId] = useState<number>(
+    Relations?.[0]?.PersonId || 0
+  );
+
+  const { data: personInfomation, refetch } = useFetchPersonInfo(selectedId);
+  console.log("personInfo=>", personInfomation);
+
   useEffect(() => {
     // Update triggerVisible if Relations changes
     setTriggerVisible(
@@ -59,9 +247,16 @@ const DetailInformation: React.FC<DetailInformationProps> = ({ data }) => {
           )
         : {}
     );
+    setSelectedId(Relations?.[0]?.PersonId);
   }, [Relations]);
 
-  const handleTriggerClick = (item: string) => {
+  useEffect(() => {
+    if (selectedId) {
+      refetch();
+    }
+  }, [selectedId, refetch]);
+
+  const handleTriggerClick = (item: string, id: number) => {
     setOpenItem(item);
     setTriggerVisible((prev) => {
       const newState = { ...prev };
@@ -70,6 +265,7 @@ const DetailInformation: React.FC<DetailInformationProps> = ({ data }) => {
       });
       return newState;
     });
+    setSelectedId(id);
   };
 
   const handleProprietor = () => {
@@ -98,22 +294,130 @@ const DetailInformation: React.FC<DetailInformationProps> = ({ data }) => {
             <AccordionItem key={relation.Id} value={`item-${index}`}>
               {triggerVisible[`item-${index}`] && (
                 <AccordionTrigger
-                  onClick={() => handleTriggerClick(`item-${index}`)}
+                  className="hover:no-underline"
+                  onClick={() =>
+                    handleTriggerClick(`item-${index}`, relation.PersonId)
+                  }
                 >
-                  {relation.PersonName}
+                  <div className="flex items-center justify-between gap-2 w-full">
+                    <h2 className="text-card-foreground font-sans text-sm font-normal leading-5 flex-1">
+                      {relation.PersonName}
+                    </h2>
+                    <div className="flex items-center gap-2">
+                      <Badge
+                        variant="secondary"
+                        className="px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+                      >
+                        {relation.RelationTypeNameEN}
+                      </Badge>
+                    </div>
+                  </div>
                 </AccordionTrigger>
               )}
               <AccordionContent>
-                {`item-${index}`} {triggerVisible[`item-${index}`]}
-                <p>
-                  <strong>Relation Type:</strong> {relation.RelationTypeNameEN}
-                </p>
-                <p>
-                  <strong>Email:</strong> {relation.PersonEmail}
-                </p>
-                <p>
-                  <strong>Phone:</strong> {relation.PhoneNumber1}
-                </p>
+                <div className="flex flex-col gap-5 py-5">
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      src="/images/avatar.png"
+                      alt="avatar"
+                      width={48}
+                      height={48}
+                    />
+                    <div className="flex flex-col gap-1 align-start">
+                      <h2 className="text-card-foreground text-base font-semibold leading-normal font-sans">
+                        {relation.PersonName}
+                      </h2>
+                      <Badge
+                        variant="secondary"
+                        className="w-[54px] text-center px-2.5 py-0.5 border rounded-md border-transparent bg-secondary"
+                      >
+                        {relation.RelationTypeNameEN}
+                      </Badge>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-3 align-start">
+                    <div className="flex items-center gap-4">
+                      <h3 className="text-muted-foreground font-sans text-base font-normal leading-normal">
+                        Email
+                      </h3>
+                      <p className="text-primary font-sans text-base font-normal leading-normal">
+                        {relation.PersonEmail}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                      <h3 className="text-muted-foreground font-sans text-base font-normal leading-normal">
+                        Phone
+                      </h3>
+                      <p className="text-primary font-sans text-base font-normal leading-normal">
+                        {relation.PhoneNumber1}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-2 justify-between">
+                    <div className="px-4 py-2 border rounded-md shadow-md">
+                      <Mail width={33} height={20} />
+                    </div>
+                    <div className="px-4 py-2 border rounded-md shadow-md">
+                      <Phone width={33} height={20} />
+                    </div>
+                    <div className="px-4 py-2 border rounded-md shadow-md">
+                      <Eye width={33} height={20} />
+                    </div>
+                    <div className="px-4 py-2 border rounded-md shadow-md">
+                      <Pencil width={33} height={20} />
+                    </div>
+                  </div>
+                  <Separator className="border" />
+                  <h2 className="text-card-foreground text-base font-semibold leading-normal font-sans">
+                    Information
+                  </h2>
+                  <div className="flex flex-col gap-3 align-start">
+                    <div className="flex align-center gap-4 justify-between">
+                      <h3 className="text-muted-foreground font-sans text-base font-normal leading-normal flex-1">
+                        Letter title
+                      </h3>
+                      <h3 className="text-primary font-sans text-base font-normal leading-normal flex-1-6">
+                        {personInfomation?.LetterTitle}
+                      </h3>
+                    </div>
+                    <div className="flex align-center gap-4 justify-between">
+                      <h3 className="text-muted-foreground font-sans text-base font-normal leading-normal flex-1">
+                        Address title
+                      </h3>
+                      <h3 className="text-primary font-sans text-base font-normal leading-normal flex-1-6">
+                        {personInfomation?.AddressTitle}
+                      </h3>
+                    </div>
+                    <div className="flex align-center gap-4 justify-between">
+                      <h3 className="text-muted-foreground font-sans text-base font-normal leading-normal flex-1">
+                        Language
+                      </h3>
+                      <h3 className="text-primary font-sans text-base font-normal leading-normal flex-1-6">
+                        {
+                          languageEnum[
+                            personInfomation?.Language as keyof typeof languageEnum
+                          ]
+                        }
+                      </h3>
+                    </div>
+                    <div className="flex align-center gap-4 justify-between">
+                      <h3 className="text-muted-foreground font-sans text-base font-normal leading-normal flex-1">
+                        Category
+                      </h3>
+                      <h3 className="text-primary font-sans text-base font-normal leading-normal flex-1-6">
+                        {personInfomation?.CategoriesDesc}
+                      </h3>
+                    </div>
+                    <div className="flex align-center gap-4 justify-between">
+                      <h3 className="text-muted-foreground font-sans text-base font-normal leading-normal flex-1">
+                        Lead Source
+                      </h3>
+                      <h3 className="text-primary font-sans text-base font-normal leading-normal flex-1-6">
+                        {personInfomation?.OriginContactInfo.NameEN}
+                      </h3>
+                    </div>
+                  </div>
+                </div>
               </AccordionContent>
             </AccordionItem>
           ))}
